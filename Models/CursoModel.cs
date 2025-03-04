56d71de3810dad68e72ca00a6513ca6c7e412b6c
victor gomes <victorgomesdacosta18@gmail.com>
2025-03-04 15:45:05 -0300

    feat: create change models and edit routes Put

--- Models/CursoModel.cs
+++ Models/CursoModel.cs
@@ -2,7 +2,7 @@ namespace Cursosufc.Models;
 
 
 public class CursoModel{
-    public CursoModel(string nomeCurso, string descricao, string duracao, string modalidade){
+    public CursoModel(string nomeCurso, string descricao, int duracao, string modalidade){
         NomeCurso = nomeCurso;
         Descricao = descricao;
         Duracao = duracao;
@@ -13,6 +13,22 @@ public class CursoModel{
     public Guid Id { get; set; }
     public string NomeCurso { get; set; }
     public string Descricao { get; set; }
-    public string Duracao { get; set; }
+    public int Duracao { get; set; }
     public string Modalidade { get; set; }
+
+    public void ChangeNomeCurso(string nomeCurso){
+        NomeCurso = nomeCurso;
+    }
+
+    public void ChangeDescricao(string descricao){
+        Descricao = descricao;
+    }
+
+    public void ChangeDuracao(int duracao){
+        Duracao = duracao;
+    }
+
+    public void ChangeModalidade(string modalidade){
+        Modalidade = modalidade;
+    }
 }
