be8b89534a447405b3549f5e20d293bc9988b02b
victor gomes <victorgomesdacosta18@gmail.com>
2025-03-08 23:27:43 -0300

    feat: rfeact user swager

--- Program.cs
+++ Program.cs
@@ -1,6 +1,40 @@
+using Cursosufc.Data;
+using Cursosufc.Routes;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<CursoContext>();
+
+builder.Services.AddControllers().AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.PropertyNamingPolicy = null;
+});
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAllOrigins",
+        policy =>
+        {
+            policy.AllowAnyOrigin()
+                  .AllowAnyMethod()
+                  .AllowAnyHeader();
+        });
+});
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+<DivideByZeroException>
 
+app.MapControllers();
+app.UseCors("AllowAllOrigins");
+
+app.UseHttpsRedirection();
+app.CursoRoutes();
 app.Run();
